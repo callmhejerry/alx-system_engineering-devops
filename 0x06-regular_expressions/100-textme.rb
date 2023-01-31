f646fec25fb97bb70a34485c685329e25652b232
callmhejerry <chinedujeremiah2002@gmail.com>
2023-02-01 00:47:24 +0100

    hot fix

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/((?<=to:).?\d*|(?<=from:)\b\w*\b|(?<=flags:)[-:0-9]*)/).join(,)
+puts ARGV[0].scan(/((?<=to:).?\d*|(?<=from:)[a-zA-Z0-9]*|(?<=flags:)[-:0-9]*)/).join(,)
