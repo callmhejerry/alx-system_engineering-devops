3e6a8a009d9a911c5d7265d04db2b7fea11fe0d8
callmhejerry <chinedujeremiah2002@gmail.com>
2023-04-10 13:35:38 +0100

    added the puppet configuration of the nginx server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,52 @@
+package { 'nginx':
+  name		   => 'nginx',
+  ensure	   => installed,
+  install_options  => ['-y'],
+  provider         => 'apt',
+}
+
+service { 'nginx':
+  name		=> 'nginx',
+  ensure	=> running,
+  hasrestart    => true,
+  restart	=> 'sudo service nginx restart',
+  require	=> [
+    Package['nginx'],
+    File['default_server'],
+    File['homepage'],
+  ],
+}
+
+$server_content = "
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+	rewrite ^/redirect_me \"https://github.com\" permanent;
+	
+	index index.html index.htm;
+
+	location /{
+		try_files \$uri \$uri/ =404;
+	}
+	server_name _;
+}
+"
+
+
+file { 'default_server':
+  ensure	=> present,
+  path		=> '/etc/nginx/sites-available/default',
+  content	=> $server_content,
+  replace	=> true,
+  require	=> File['homepage'],
+}
+
+
+file { 'homepage':
+  ensure	=> present,
+  path		=> '/var/www/html/index.html',
+  content	=> "Hello World!\n",
+  replace	=> true,
+}
